54d0bbd59635ef73ce1632649afaef431c50f13c
Leina <mtleina@outlook.fr>
2026-02-26 11:08:05 -0400

    configuration api.php et User.php

--- project/flutter_bank_app/routes/api.php
+++ project/flutter_bank_app/routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+//"INSCRIPTION"
+Route::post('/register', [AuthController::class, 'register']);
+
+//"LOGIN"
+Route::post('/login', [AuthController::class, 'login']);
+
+//pour voir ses propres infos (ton "DASHBOARD")
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
